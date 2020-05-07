46fb3647b05ca73d7121d7eea0fc4ae5ef53ecc3
pavitra mohanty <pavitramohanty111@gmail.com>
2020-05-06 20:22:51 -0400

    Update urls.py

--- weather/urls.py
+++ weather/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='hom'),
+    path('',views.index,name='home'),
     path('delete/<city_name>/',views.delete_city, name='delete_city')
 ]
